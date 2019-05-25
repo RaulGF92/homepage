f957fbdc69df204807943598d5136663c775b274
Raúl García <raulgf92@gmail.com>
2019-05-25 02:21:31 +0200

    Terminada primera versión de Experiencia

---
+++ src/components/laboralComponent/timeline/TimelineComponent.js
@@ -0,0 +1,10 @@
+import Component from '../../../Component';
+import './TimelineComponent.scss';
+
+export default class TimelineComponent extends Component {
+
+    constructor() {
+        super(TimelineComponent,"#timeline");
+    }
+
+}
