2dc40a02e0de7c61fb18d2c698376251651aa1f4
Raúl García <raulgf92@gmail.com>
2020-04-04 13:47:06 +0200

    Añadida animación a expansion de componentes

--- src/components/laboralComponent/timeline/timelineItem/TimelineItemComponent.js
+++ src/components/laboralComponent/timeline/timelineItem/TimelineItemComponent.js
@@ -7,7 +7,17 @@ export default class TimelineItemComponent extends Component {
     }
 
     onClick(_this, element, event) {
-        debugger
+        _this.parentElement.querySelectorAll(".timeline-item.active")
+        .forEach(element => {
+            element.classList.remove("active") 
+            element.style.maxHeight = "150px"   
+        });
+        _this.classList.add("active")
+
+        const maxHeight = _this.offsetHeight
+        const p  = _this.querySelector("p");
+        const auxHeight = maxHeight + p.scrollHeight 
+        _this.style.maxHeight = `${auxHeight}px` 
     }
 
     visualizar() {
